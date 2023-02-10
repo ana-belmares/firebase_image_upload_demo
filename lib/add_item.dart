e683c572c05d515366b44dd30675b68058993e04
ana-belmares <asofiabg@hotmail.com>
2023-02-09 18:05:37 -0600

    first commit

---
+++ lib/add_item.dart
@@ -0,0 +1,145 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class AddItem extends StatefulWidget {
+  const AddItem({Key? key}) : super(key: key);
+
+  @override
+  State<AddItem> createState() => _AddItemState();
+}
+
+class _AddItemState extends State<AddItem> {
+  TextEditingController _controllerName = TextEditingController();
+
+  GlobalKey<FormState> key = GlobalKey();
+
+  CollectionReference _reference =
+      FirebaseFirestore.instance.collection('image_list');
+
+  String imageUrl = '';
+  XFile? file;
+  String uniqueFileName = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add an image'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: key,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _controllerName,
+                decoration:
+                    InputDecoration(hintText: 'Enter the name of the image'),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter the image name';
+                  }
+
+                  return null;
+                },
+              ),
+              IconButton(
+                  onPressed: () async {
+                    /*
+                * Step 1. Pick/Capture an image   (image_picker)
+                * Step 2. Upload the image to Firebase storage
+                * Step 3. Get the URL of the uploaded image
+                * Step 4. Store the image URL inside the corresponding
+                *         document of the database.
+                * Step 5. Display the image on the list
+                *
+                * */
+
+                    /*Step 1:Pick image*/
+                    //Install image_picker
+                    //Import the corresponding library
+
+                    ImagePicker imagePicker = ImagePicker();
+                    file =
+                        await imagePicker.pickImage(source: ImageSource.camera);
+                    print('${file?.path}');
+
+                    if (file == null) return;
+                    //Import dart:core
+                    uniqueFileName =
+                        DateTime.now().millisecondsSinceEpoch.toString();
+
+                    /*Step 2: Upload to Firebase storage*/
+                    //Install firebase_storage
+                    //Import the library
+                  },
+                  icon: Icon(Icons.camera_alt)),
+                  IconButton(
+                  onPressed: () async {
+
+                    ImagePicker imagePicker = ImagePicker();
+                    file =
+                        await imagePicker.pickImage(source: ImageSource.gallery);
+                    print('${file?.path}');
+
+                    if (file == null) return;
+                    //Import dart:core
+                    uniqueFileName =
+                        DateTime.now().millisecondsSinceEpoch.toString();
+
+                  },
+                  icon: Icon(Icons.image)),
+              ElevatedButton(
+                  onPressed: () async {
+                    //Get a reference to storage root
+                    Reference referenceRoot = FirebaseStorage.instance.ref();
+                    Reference referenceDirImages =
+                        referenceRoot.child('images');
+
+                    //Create a reference for the image to be stored
+                    Reference referenceImageToUpload =
+                        referenceDirImages.child(uniqueFileName);
+
+                    //Handle errors/success
+                    try {
+                      //Store the file
+                      await referenceImageToUpload.putFile(File(file!.path));
+                      //Success: get the download URL
+                      imageUrl = await referenceImageToUpload.getDownloadURL();
+                    } catch (error) {
+                      //Some error occurred
+                    }
+
+                    if (imageUrl.isEmpty) {
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(SnackBar(content: Text('Please upload an image')));
+
+                      return;
+                    }
+
+                    if (key.currentState!.validate()) {
+                      String itemName = _controllerName.text;
+
+                      //Create a Map of data
+                      Map<String, String> dataToSend = {
+                        'name': itemName,
+                        'image': imageUrl,
+                      };
+
+                      //Add a new item
+                      _reference.add(dataToSend);
+                    }
+                  },
+                  child: Text('Submit'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/edit_item.dart
@@ -0,0 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class EditItem extends StatelessWidget {
+  EditItem(this._shoppingItem, {Key? key}) {
+    _controllerName = TextEditingController(text: _shoppingItem['name']);
+
+    _reference = FirebaseFirestore.instance
+        .collection('image_list')
+        .doc(_shoppingItem['id']);
+  }
+
+  Map _shoppingItem;
+  late DocumentReference _reference;
+
+  late TextEditingController _controllerName;
+  GlobalKey<FormState> _key = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit an item'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _key,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _controllerName,
+                decoration:
+                    InputDecoration(hintText: 'Enter the name of the item'),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter the item name';
+                  }
+
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    if (_key.currentState!.validate()) {
+                      String name = _controllerName.text;
+
+                      //Create the Map of data
+                      Map<String,String> dataToUpdate={
+                        'name':name,
+                      };
+
+                      //Call update()
+                      _reference.update(dataToUpdate);
+                    }
+                  },
+                  child: Text('Submit'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
